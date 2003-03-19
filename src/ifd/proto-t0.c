328ea27b69d9dec66d9e8e42fc768926f2f73381
okir <okir>
2003-03-19 12:37:50 +0000

    - more T=0 changes

--- src/ifd/proto-t0.c
+++ src/ifd/proto-t0.c
@@ -24,9 +24,10 @@ enum {
 	IDLE, SENDING, RECEIVING, CONFUSED
 };
 
-static int		t0_resynch(ifd_reader_t *);
-static int		t0_send(ifd_device_t *, ifd_buf_t *, int);
-static int		t0_recv(ifd_device_t *, ifd_buf_t *, int, long);
+static int	t0_xcv(ifd_device_t *, t0_params_t *, ifd_apdu_t *);
+static int	t0_send(ifd_device_t *, ifd_buf_t *, int);
+static int	t0_recv(ifd_device_t *, ifd_buf_t *, int, long);
+static int	t0_resynch(ifd_reader_t *);
 
 /*
  * Set default T=1 protocol parameters
@@ -122,10 +123,9 @@ t0_transceive(ifd_reader_t *reader, unsigned char nad, ifd_apdu_t *apdu)
 {
 	ifd_device_t	*dev = reader->device;
 	t0_params_t	*t0 = t0_params(reader);
-	ifd_buf_t	sbuf, rbuf;
+	ifd_apdu_t	tpdu;
 	unsigned char	sdata[5];
-	unsigned int	ins, lc, le;
-	unsigned int	null_count = 0;
+	unsigned int	cla, cse, lc, le;
 
 	if (t0->state != IDLE) {
 		if (t0_resynch(reader) < 0)
@@ -133,8 +133,97 @@ t0_transceive(ifd_reader_t *reader, unsigned char nad, ifd_apdu_t *apdu)
 		t0->state = IDLE;
 	}
 
-	if (apdu->snd_len < 4)
+	if (apdu->snd_len < 4 || apdu->rcv_len < 2)
 		return -1;
+
+	/* Copy APDU */
+	tpdu = *apdu;
+
+	/* Get class byte */
+	cla = ((unsigned char *) apdu->rcv_buf)[0];
+
+	/* Check the APDU case */
+	cse = ifd_apdu_case(&tpdu, &lc, &le);
+
+	switch (cse) {
+	case IFD_APDU_CASE_1:
+	case IFD_APDU_CASE_2S:
+	case IFD_APDU_CASE_3S:
+		break;
+	case IFD_APDU_CASE_4S:
+		/* Strip off the Le byte */
+		tpdu.snd_len--;
+		break;
+	default:
+		/* We don't handle ext APDUs */
+		return -1;
+	}
+
+	if (le + 2 > tpdu.rcv_len) {
+		ifd_error("t0_transceive: recv buffer too small");
+		return -1;
+	}
+
+	if (lc) {
+		t0->state = SENDING;
+		tpdu.rcv_len = 2;
+
+		if (t0_xcv(dev, t0, &tpdu) < 0)
+			return -1;
+
+		/* Can this happen? */
+		if (tpdu.rcv_len != 2)
+			return -1;
+
+		/* Case 4 APDU - check whether we should
+		 * try to get the response */
+		if (cse == IFD_APDU_CASE_4S) {
+			unsigned char	*sw;
+
+			sw = (unsigned char *) tpdu.rcv_buf;
+
+			sdata[0] = cla;
+			sdata[1] = 0xC0;
+			sdata[2] = 0x00;
+			sdata[3] = 0x00;
+			sdata[4] = le;
+
+			if (sw[0] == 0x61) {
+				/* additional length info */
+				if (sw[1] != 0 && sw[1] < le)
+					sdata[4] = sw[1];
+			} else if ((sw[0] & 0xF0) == 0x60) {
+				/* Command not accepted, do not
+				 * retrieve response
+				 */
+				goto done;
+			}
+
+			/* Transmit a Get Response command */
+			tpdu.snd_buf = sdata;
+			tpdu.snd_len = 5;
+		} else {
+			goto done;
+		}
+	}
+
+	t0->state = RECEIVING;
+	tpdu.rcv_len = le + 2;
+	if (t0_xcv(dev, t0, &tpdu) < 0)
+		return -1;
+
+done:	t0->state = IDLE;
+	apdu->rcv_len = tpdu.rcv_len;
+	return 0;
+}
+
+static int
+t0_xcv(ifd_device_t *dev, t0_params_t *t0, ifd_apdu_t *apdu)
+{
+	ifd_buf_t	sbuf, rbuf;
+	unsigned char	sdata[5];
+	unsigned int	ins, lc, le;
+	unsigned int	null_count = 0;
 
 	/* Initialize send/recv buffers.
 	 * The receive buffer is initialized to exactly the number of
@@ -149,6 +238,12 @@ t0_transceive(ifd_reader_t *reader, unsigned char nad, ifd_apdu_t *apdu)
 		/* Extract lc and le */
 		lc = 0;
 		le = sdata[4];
+
+		/* Depending on the APDU case, an Le of 0 may
+		 * mean 256 in fact.
+		 */
+		if (le == 0 && apdu->rcv_len >= 258)
+			le = 256;
 
 		t0->state = RECEIVING;
 	} else {
