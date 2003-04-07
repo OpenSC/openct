cb646f1ea6e7ec98caea4df98c0b56d54a891edc
okir <okir>
2003-04-07 11:01:44 +0000

    - added a few error codes

--- src/include/openct/error.h
+++ src/include/openct/error.h
@@ -11,10 +11,17 @@
 #define IFD_ERROR_GENERIC	-1
 #define IFD_ERROR_TIMEOUT	-2
 #define IFD_ERROR_INVALID_SLOT	-3
+#define IFD_ERROR_NOT_SUPPORTED	-4
+#define IFD_ERROR_COMM_ERROR	-5
+#define IFD_ERROR_NO_CARD	-6
+#define IFD_ERROR_LOCKED	-7
+#define IFD_ERROR_NOLOCK	-8
 
 /* for application/resource manager protocol */
 #define IFD_ERROR_INVALID_MSG	-100
 #define IFD_ERROR_INVALID_CMD	-101
+#define IFD_ERROR_MISSING_ARG	-102
+#define IFD_ERROR_INVALID_ARG	-103
 
 extern const char *	ifd_hexdump(const unsigned char *, size_t);
 
