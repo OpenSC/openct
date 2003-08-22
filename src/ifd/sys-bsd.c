bbd338ba773ab2463a22cab758de2ee33b334d81
Andreas Jellinghaus <andreas@ionisiert.de>
2003-08-22 13:26:49 +0000

    fix typo in email addresses.

--- src/ifd/sys-bsd.c
+++ src/ifd/sys-bsd.c
@@ -2,8 +2,8 @@
  * *BSD specific functions
  *
  * Copyright (C) 2003 Olaf Kirch <okir@suse.de>
- * Copyright (C) 2003 Andreas Jellinghaus <aj@suse.de>
- * Copyright (C) 2003 Markus Friedl <aj@suse.de>
+ * Copyright (C) 2003 Andreas Jellinghaus <aj@dungeon.inka.de>
+ * Copyright (C) 2003 Markus Friedl <markus@openbsd.org>
  *
  * These functions need to be re-implemented for every
  * new platform.
