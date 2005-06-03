77fa49e213794680bec17b29ef379c16bbfb0ffc
Andreas Jellinghaus <andreas@ionisiert.de>
2005-06-03 20:28:43 +0000

    accept more parameters, e.g. estonian id card.

--- src/ifd/atr.h
+++ src/ifd/atr.h
@@ -13,9 +13,9 @@ extern "C" {
 
 	typedef struct ifd_atr_info {
 		/* The following contain -1 if the field wasn't present */
-		int TA[3];
-		int TB[3];
-		int TC[3];
+		int TA[4];
+		int TB[4];
+		int TC[4];
 		unsigned int supported_protocols;
 		int default_protocol;
 	} ifd_atr_info_t;
