a9e1f37addee183f01fb7c469d1348e88f8b518d
Andreas Jellinghaus <andreas@ionisiert.de>
2006-11-02 07:56:46 +0000

    fix from freebsd cvs.

--- src/ifd/ifd-wbeiuu.c
+++ src/ifd/ifd-wbeiuu.c
@@ -454,6 +454,7 @@ static int wbeiuu_card_reset(ifd_reader_t * reader, int slot, void *atr,
 {
 	int status;
 	unsigned char buf[256];
+	unsigned char len;
 	ifd_device_t *dev;
 
 	ifd_debug(1, "%s:%d wbeiuu_card_reset()", __FILE__, __LINE__);
@@ -503,8 +504,6 @@ static int wbeiuu_card_reset(ifd_reader_t * reader, int slot, void *atr,
 			  status);
 		return -1;
 	}
-	//unsigned char len[1] = { 0x00 };
-	unsigned char len;
 	ifd_debug(1, "%s:%d How many bytes waiting at the FIFO?", __FILE__,
 		  __LINE__, status);
 	status = ifd_sysdep_usb_bulk(dev, 0x82, &len, 1, USB_TIMEOUT);
